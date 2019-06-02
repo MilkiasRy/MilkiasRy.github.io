5baaa6803c09944d22315c313a56113da347f457
MILKIAS GHEBREMARIAM <milkias@milkiass-mbp.cs.mum.edu>
2019-06-01 22:05:20 -0500

    intial

---
+++ lab4/buggy_page2/bugg2.js
@@ -0,0 +1,4 @@
+
+function gogogo() {
+    alert("Yay, it works!");
+}
---
+++ lab4/buggy_page4/buggy4.js
@@ -0,0 +1,13 @@
+
+/*function somethingElse() {
+    if (5 > 2) {
+        alert("Bigger");
+    if (5 < 2) {
+        alert("Smaller");
+    }
+}*/
+
+function gogogo() {
+    alert("Yay, it works!");
+}
+//}
