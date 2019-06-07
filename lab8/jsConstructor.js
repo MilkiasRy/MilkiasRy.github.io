2816a9bdf24e5f47787be5005088ad4c11100938
MILKIAS GHEBREMARIAM <milkias@MILKIASs-MacBook-Pro.local>
2019-06-07 01:12:06 -0500

    bvin

---
+++ lab8/jsConstructor.js
@@ -0,0 +1,1 @@
+Assignment-8_JS_Inheritance
---
+++ lab8/js_inheritance.js
@@ -0,0 +1,49 @@
+
+var objectCreators=(function(){
+    "use strict";
+    var createBicylePrototype=function(){
+        return {
+            speed:0,
+            applyBrake:function(x){
+                this.speed=this.speed-x;
+            },
+            speedUp:function(x){
+                this.speed=this.speed+x;
+            }
+        };
+    }
+    var createMountainBikeProtype=function(){
+        var newObj=Object.create(x);
+        newObj.gear=1;
+        newObj.setGear=function(x){
+            this.gear=x;
+        };
+        return newObj;
+    }
+    var start=function(){
+        var bicycle=createBicyclePrototype();
+    console.log(bicycle.speed);
+    bicycle.speedUp(5);
+    bicycle.applyBrake(2);
+     console.log(bicycle.speed);
+    var mountainBike=createMountainBikePrototype(bicycle);
+    mountainBike.setGear(30);
+     console.log(mountainBike.speed);
+    
+}
+
+start();
+
+var biycle1=Object.create(createBicyclePrototype());
+console.log(biycle1.speed);
+    biycle1.speedUp(10);
+    biycle1.applyBrake(2);
+     console.log(biycle1.speed);
+
+var mountain1=Object.create(createMountainBikePrototype(biycle1));
+mountain1.setGear(2);
+console.log(mountain1.gear);
+mountain1.speedUp(5);
+console.log(mountain1.speed);
+    
+})();
