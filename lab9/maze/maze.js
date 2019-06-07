15e7234b41edb90cd402f5409321e9b9872e13e6
MILKIAS GHEBREMARIAM <milkias@milkiass-mbp.cs.mum.edu>
2019-06-07 09:57:32 -0500

    in

---
+++ lab9/maze/maze.js
@@ -0,0 +1,39 @@
+let rice=false;
+$(document).ready(function(){
+    $("#boundary1").mouseover(gettingRed);
+    $("#boundary2").mouseover(gettingRed);
+    $("#boundary3").mouseover(gettingRed);
+    $("#boundary4").mouseover(gettingRed);
+    $("#boundary5").mouseover(gettingRed);
+    $("#start").click(start);
+    $("#end").click(end);
+
+
+});
+function gettingRed(evnt){
+    if(rice){
+        $(this).css("background-color","#ff8888").addClass("hovered");
+    }
+}
+function start(evt) {
+  rice = true;
+    $("div.boundary").removeClass("hovered").css("background-color", "#eeeeee");
+    $("#status").text('Click the "S" to begin.');
+    return rice;
+}
+function end(evt) {
+    if (rice) {
+       rice = false;
+        if (checkResult()) {
+            $("#status").text('You lose! Click the "S" to begin again.');
+        } else {
+            $("#status").text('You win :). Click the "S" to begin again.');
+        }
+    } else {
+        alert('Click the "S" to begin.');
+    }
+}
+
+function checkResult() {
+    return $("div").hasClass("hovered");
+}
